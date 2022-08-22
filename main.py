f328940c5058e43e8f0db1defee336544761f1a3
EduardoBojer98 <ediboieru@gmail.com>
2022-08-23 00:08:08 +0200

    Update hole project

--- main.py
+++ main.py
@@ -3,83 +3,74 @@ from tkinter import *
 from tkinter import ttk
 from tkinter import messagebox
 import tkinter as tk
-import operations
+from operations import CRUDOP
 
-# gui
+
+class MainFrame:
+    crud=
+    def __init__(self, root):
+        self.root = root
+        self.root.geometry("1350x700+10+10")
+        self.root.title("Inventory System EdivetPlus | SI6/17")
+        self.my_tree = ttk.Treeview(root)
+
+        title = Label(self.root, text="IMS EdivetPlus", font=('Arial Bold', 25), bg="#67B7D1", fg='white', anchor="w",padx=20).place(x=0, y=0, relwidth=1, height=60)
+
+        btn_logout = Button(self.root, text="Logout", font=("Arial", 15), bg='red', fg='white', cursor='hand2').place(x=1200, y=15, height=35, width=120)
+
+        self.idlabel = Label(self.root, text="ItemID", font=('Arial', 15))
+        self.namelabel = Label(self.root, text="Name", font=('Arial', 15))
+        self.pricelabel = Label(self.root, text="Price", font=('Arial', 15))
+        self.quantitylabel = Label(self.root, text="Quantity", font=('Arial', 15))
+        self.manufacturerlabel = Label(self.root, text="Manufacturer", font=('Arial', 15))
+        self.idlabel.place(x=50, y=100, height=50, width=150)
+        self.namelabel.place(x=50, y=150, height=50, width=150)
+        self.pricelabel.place(x=50, y=200, height=50, width=150)
+        self.quantitylabel.place(x=50, y=250, height=50, width=150)
+        self.manufacturerlabel.place(x=50, y=300, height=50, width=150)
+
+        self.itemEntry = Entry(root, width=55, bd=5, font=('Arial', 15))
+        self.nameEntry = Entry(root, width=55, bd=5, font=('Arial', 15))
+        self.priceEntry = Entry(root, width=55, bd=5, font=('Arial', 15))
+        self.quantityEntry = Entry(root, width=55, bd=5, font=('Arial', 15))
+        self.manufacturerEntry = Entry(root, width=55, bd=5, font=('Arial', 15))
+        self.itemEntry.place(x=200, y=100, height=50, width=400)
+        self.nameEntry.place(x=200, y=150, height=50, width=400)
+        self.priceEntry.place(x=200, y=200, height=50, width=400)
+        self.quantityEntry.place(x=200, y=250, height=50, width=400)
+        self.manufacturerEntry.place(x=200, y=300, height=50, width=400)
+
+        self.addBtn = Button(root, text="Add", padx=65, pady=25, width=10, bd=5, font=('Arial', 15), bg="#84F894")
+        self.addBtn.place(x=750, y=70)
+        self.deleteBtn = Button(root, text="Delete", padx=65, pady=25, width=10, bd=5, font=('Arial', 15), bg="#FF9999")
+        self.deleteBtn.place(x=750, y=170)
+        self.searchBtn = Button(root, text="Search", padx=65, pady=25, width=10, bd=5, font=('Arial', 15), bg="#F4FE82")
+        self.searchBtn.place(x=750, y=270)
+        self.updateBtn = Button(root, text="Update", padx=65, pady=25, width=10, bd=5, font=('Arial', 15), bg="#84E8F8")
+        self.updateBtn.place(x=1050, y=70)
+        self.resetBtn = Button(root, text="Reset", padx=65, pady=25, width=10, bd=5, font=('Arial', 15), bg="#F398FF")
+        self.resetBtn.place(x=1050, y=170)
+        self.selectBtn = Button(root, text="Select", padx=65, pady=25, width=10, bd=5, font=('Arial', 15), bg="#EEEEEE")
+        self.selectBtn.place(x=1050, y=270)
+
+        self.style = ttk.Style()
+        self.style.configure("Treeview.Heading", font=('Arial Bold', 15))
+        self.my_tree['columns'] = ("ID", "Name", "Price", "Quantity", "Manufacturer")
+        self.my_tree.column('#0', width=0, stretch=NO)
+        self.my_tree.column("ID", anchor=W, width=170)
+        self.my_tree.column("Name", anchor=W, width=150)
+        self.my_tree.column("Price", anchor=W, width=150)
+        self.my_tree.column("Quantity", anchor=W, width=165)
+        self.my_tree.column("Manufacturer", anchor=W, width=150)
+        self.my_tree.heading("ID", text="Item ID", anchor=W)
+        self.my_tree.heading("Name", text="Item Name", anchor=W)
+        self.my_tree.heading("Price", text="Item Price", anchor=W)
+        self.my_tree.heading("Quantity", text="Item Quantity", anchor=W)
+        self.my_tree.heading("Manufacturer", text="Item Manufacturer", anchor=W)
+
+        refreshTable(self.my_tree)
+
+
 root = Tk()
-root.title("Inventory System EdivetPlus")
-root.geometry("1130x750")
-my_tree = ttk.Treeview(root)
-
-def refreshTable():
-    for data in my_tree.get_children():
-        my_tree.delete(data)
-
-    for array in operations.read():
-        my_tree.insert(parent='', index='end', iid=array, text="", values=(array), tag='orow')
-
-    my_tree.tag_configure('orow', background="#EEEEEE", font=('Arial', 12))
-    my_tree.grid(row=8, column=0, columnspan=5, rowspan=11, padx=10, pady=20)
-
-
-
-label = Label(root, text="Inventory System EdivetPlus", font=('Arial Bold', 25))
-label.grid(row=0, column=0, columnspan=8, rowspan=2, padx=50, pady=40)
-
-idlabel = Label(root, text="ItemID", font=('Arial', 15))
-namelabel = Label(root, text="Name", font=('Arial', 15))
-pricelabel = Label(root, text="Price", font=('Arial', 15))
-quantitylabel = Label(root, text="Quantity", font=('Arial', 15))
-manufacturerlabel = Label(root, text="Manufacturer", font=('Arial', 15))
-
-idlabel.grid(row=3, column=0, columnspan=1, padx=50, pady=5)
-namelabel.grid(row=4, column=0, columnspan=1, padx=50, pady=5)
-pricelabel.grid(row=5, column=0, columnspan=1, padx=50, pady=5)
-quantitylabel.grid(row=6, column=0, columnspan=1, padx=50, pady=5)
-manufacturerlabel.grid(row=7, column=0, columnspan=1, padx=50, pady=5)
-
-itemEntry = Entry(root, width=55, bd=5, font=('Arial', 15))
-nameEntry = Entry(root, width=55, bd=5, font=('Arial', 15))
-priceEntry = Entry(root, width=55, bd=5, font=('Arial', 15))
-quantityEntry = Entry(root, width=55, bd=5, font=('Arial', 15))
-manufacturerEntry = Entry(root, width=55, bd=5, font=('Arial', 15))
-
-itemEntry.grid(row=3, column=1, columnspan=4, padx=5, pady=0)
-nameEntry.grid(row=4, column=1, columnspan=4, padx=5, pady=0)
-priceEntry.grid(row=5, column=1, columnspan=4, padx=5, pady=0)
-quantityEntry.grid(row=6, column=1, columnspan=4, padx=5, pady=0)
-manufacturerEntry.grid(row=7, column=1, columnspan=4, padx=5, pady=0)
-
-addBtn = Button(root, text="Add", padx=65, pady=25, width=10, bd=5, font=('Arial', 15), bg="#84F894")
-updateBtn = Button(root, text="Update", padx=65, pady=25, width=10, bd=5, font=('Arial', 15), bg="#84E8F8")
-deleteBtn = Button(root, text="Delete", padx=65, pady=25, width=10, bd=5, font=('Arial', 15), bg="#FF9999")
-searchBtn = Button(root, text="Search", padx=65, pady=25, width=10, bd=5, font=('Arial', 15), bg="#F4FE82")
-resetBtn = Button(root, text="Reset", padx=65, pady=25, width=10, bd=5, font=('Arial', 15), bg="#F398FF")
-selectBtn = Button(root, text="Select", padx=65, pady=25, width=10, bd=5, font=('Arial', 15), bg="#EEEEEE")
-
-addBtn.grid(row=3, column=5, columnspan=1, rowspan=2)
-updateBtn.grid(row=5, column=5, columnspan=1, rowspan=2)
-deleteBtn.grid(row=7, column=5, columnspan=1, rowspan=2)
-searchBtn.grid(row=9, column=5, columnspan=1, rowspan=2)
-resetBtn.grid(row=11, column=5, columnspan=1, rowspan=2)
-selectBtn.grid(row=13, column=5, columnspan=1, rowspan=2)
-
-style = ttk.Style()
-style.configure("Treeview.Heading", font=('Arial Bold', 15))
-my_tree['columns'] = ("ID", "Name", "Price", "Quantity", "Manufacturer")
-my_tree.column('#0', width=0, stretch=NO)
-my_tree.column("ID", anchor=W, width=170)
-my_tree.column("Name", anchor=W, width=150)
-my_tree.column("Price", anchor=W, width=150)
-my_tree.column("Quantity", anchor=W, width=165)
-my_tree.column("Manufacturer", anchor=W, width=150)
-
-my_tree.heading("ID", text="Item ID", anchor=W)
-my_tree.heading("Name", text="Item Name", anchor=W)
-my_tree.heading("Price", text="Item Price", anchor=W)
-my_tree.heading("Quantity", text="Item Quantity", anchor=W)
-my_tree.heading("Manufacturer", text="Item Manufacturer", anchor=W)
-
-refreshTable()
-
+obj = MainFrame(root)
 root.mainloop()
--- operations.py
+++ operations.py
@@ -5,19 +5,55 @@ from tkinter import messagebox
 import tkinter as tk
 
 
-def connection():
-    conn = pymysql.connect(
-        host='localhost', user='root', password='', db='data_db'
-    )
-    return conn
+class databaseconn:
+    def connection(self):
+        conn = pymysql.connect(
+            host='localhost', user='root', password='', db='data_db'
+        )
+        return conn
 
-def read():
-    conn = connection()
-    cursor = conn.cursor()
-    cursor.execute("SELECT * FROM data")
-    result = cursor.fetchall()
-    conn.commit()
-    conn.close()
-    return result
 
-def add()
+class CRUDOP:
+    def read(self):
+        conn = connection()
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM data")
+        result = cursor.fetchall()
+        conn.commit()
+        conn.close()
+        return result
+
+    def refreshTable(my_tree):
+        for data in my_tree.get_children():
+            my_tree.delete(data)
+
+        for array in read():
+            my_tree.insert(parent='', index='end', iid=array, text="", values=array)
+
+        my_tree.tag_configure('orow', background="#EEEEEE", font=('Arial', 12))
+        my_tree.grid(row=8, column=0, columnspan=5, rowspan=11, padx=10, pady=20)
+
+    def add(self, itemEntry, nameEntry, priceEntry, quantityEntry, manufacturerEntry, my_tree):
+        itemid = str(itemEntry.get())
+        itemname = str(nameEntry.get())
+        itemprice = str(priceEntry.get())
+        itemquantity = str(quantityEntry.get())
+        itemmanufacturer = str(manufacturerEntry.get())
+
+        if (itemid == "" or itemid == " ") or (itemname == "" or itemname == " ") or (
+                itemprice == "" or itemprice == " ") or (itemquantity == "" or itemquantity == " ") or (
+                itemmanufacturer == "" or itemmanufacturer == " "):
+            messagebox.showinfo("Error", "Please fill up the blank entry")
+            return
+        else:
+            try:
+                conn = connection()
+                cursor = conn.cursor()
+                cursor.execute(
+                    "INSERT INTO data VALUES ('" + itemid + "','" + itemname + "','" + itemprice + "','" + itemquantity + "','" + itemmanufacturer + "')")
+                conn.commit()
+                conn.close()
+            except:
+                messagebox.showinfo("Error", "Item ID already exist")
+                return
+        refreshTable(my_tree)
