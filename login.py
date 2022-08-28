edd5140b872804464631220be16d6b920e294d52
EduardoBojer98 <ediboieru@gmail.com>
2022-08-28 15:59:22 +0200

    Zuriranje projekta i popravke

--- login.py
+++ login.py
@@ -72,24 +72,6 @@ class Login:
                          cursor='hand2', fg='#57a1f8')
         sing_up.place(x=215, y=270)
 
-    def login(self):
-        con = sqlite3.connect(database=r'ims.db')
-        cur = con.cursor()
-        try:
-            if self.username.get() == "" or self.password.get() == "":
-                messagebox.showerror("Error", "All filds are requaierd", parent=self.root)
-            else:
-                cur.execute("select * from users where Name=? AND password=?",
-                            (self.username.get(), self.password.get()))
-                user = cur.fetchone()
-                if user == None:
-                    messagebox.showerror("Error", "Invalid username or password", parent=self.root)
-                else:
-                    self.root.destroy()
-                    os.system("python main.py")
-        except Exception as ex:
-            messagebox.showerror("Error", f"Error due to : {str(ex)}", parent=self.root)
-
     def createAcc(self):
         self.root.destroy()
         os.system("python signup.py")
--- main.py
+++ main.py
@@ -13,7 +13,7 @@ class MainFrame:
     def __init__(self, root):
         self.root = root
         self.root.geometry("1350x700+10+10")
-        self.root.title("Inventory System EdivetPlus | SI6/17")
+        self.root.title("Sistem za proizvode EdivetPlus | SI 6/17")
         self.my_tree = ttk.Treeview(root)
 
         # ============Variables
@@ -26,11 +26,11 @@ class MainFrame:
         self.var_quantity = StringVar()
         self.var_manufacturer = StringVar()
         # =========================naslov programa
-        title = Label(self.root, text="IMS EdivetPlus", font=('Arial Bold', 25), bg="#67B7D1", fg='white', anchor="w",
+        title = Label(self.root, text="EdivetPlus", font=('Arial Bold', 25), bg="#67B7D1", fg='white', anchor="w",
                       padx=20)
         title.place(x=0, y=0, relwidth=1, height=60)
 
-        btn_logout = Button(self.root,command=self.logout, text="Logout", font=("Arial", 15), bg='red', fg='white', cursor='hand2')
+        btn_logout = Button(self.root,command=self.logout, text="Odjava", font=("Arial", 15), bg='red', fg='white', cursor='hand2')
         btn_logout.place(x=1200, y=15, height=35, width=120)
 
         # ===================Search fildovi
@@ -38,7 +38,7 @@ class MainFrame:
         SearchFrame.place(x=400, y=0, width=600, height=60)
 
         sel_search = ttk.Combobox(SearchFrame, textvariable=self.var_searchType,
-                                  values=("Select", "Name", "Manufacturer"), state='readonly',
+                                  values=("Izaberi", "Ime", "Proizvodjac"), state='readonly',
                                   justify=CENTER, font=('Arial', 15))
         sel_search.place(x=5, y=0, width=180)
         sel_search.current(0)
@@ -46,16 +46,16 @@ class MainFrame:
         txt_search = Entry(SearchFrame, textvariable=self.var_searchtxt, font=('Arial', 15), bg='white')
         txt_search.place(x=200, y=0)
 
-        btn_search = Button(SearchFrame, command=self.search, text="Search", font=('Arial', 15), bg="#EEEEEE",
+        btn_search = Button(SearchFrame, command=self.search, text="Pretraga", font=('Arial', 15), bg="#EEEEEE",
                             fg='black', cursor='hand2')
         btn_search.place(x=450, y=0, width=120, height=30)
 
         # ============================imena fildova za imput
-        idlabel = Label(self.root, text="ItemID", font=('Arial', 15))
-        namelabel = Label(self.root, text="Name", font=('Arial', 15))
-        pricelabel = Label(self.root, text="Price", font=('Arial', 15))
-        quantitylabel = Label(self.root, text="Quantity", font=('Arial', 15))
-        manufacturerlabel = Label(self.root, text="Manufacturer", font=('Arial', 15))
+        idlabel = Label(self.root, text="Sifra", font=('Arial', 15))
+        namelabel = Label(self.root, text="Ime", font=('Arial', 15))
+        pricelabel = Label(self.root, text="Cena", font=('Arial', 15))
+        quantitylabel = Label(self.root, text="Kolicina", font=('Arial', 15))
+        manufacturerlabel = Label(self.root, text="Proizvodjac", font=('Arial', 15))
         idlabel.place(x=50, y=100, height=50, width=150)
         namelabel.place(x=50, y=150, height=50, width=150)
         pricelabel.place(x=50, y=200, height=50, width=150)
@@ -74,16 +74,16 @@ class MainFrame:
         quantityEntry.place(x=200, y=250, height=50, width=400)
         manufacturerEntry.place(x=200, y=300, height=50, width=400)
         # =================buttons za dodavalje obrisanje itd
-        addBtn = Button(self.root, command=self.add, text="Add", padx=65, pady=25, width=10, bd=5, font=('Arial', 15),
+        addBtn = Button(self.root, command=self.add, text="Dodaj", padx=65, pady=25, width=10, bd=5, font=('Arial', 15),
                         bg="#84F894")
         addBtn.place(x=750, y=100)
-        deleteBtn = Button(self.root, command=self.delete, text="Delete", padx=65, pady=25, width=10, bd=5,
+        deleteBtn = Button(self.root, command=self.delete, text="Obrisi", padx=65, pady=25, width=10, bd=5,
                            font=('Arial', 15), bg="#FF9999")
         deleteBtn.place(x=750, y=200)
-        updateBtn = Button(self.root, command=self.update, text="Update", padx=65, pady=25, width=10, bd=5,
+        updateBtn = Button(self.root, command=self.update, text="Azuriraj", padx=65, pady=25, width=10, bd=5,
                            font=('Arial', 15), bg="#84E8F8")
         updateBtn.place(x=1050, y=100)
-        resetBtn = Button(self.root, command=self.clear, text="Clear", padx=65, pady=25, width=10, bd=5,
+        resetBtn = Button(self.root, command=self.clear, text="Cisti", padx=65, pady=25, width=10, bd=5,
                           font=('Arial', 15), bg="#F398FF")
         resetBtn.place(x=1050, y=200)
 
@@ -93,24 +93,24 @@ class MainFrame:
 
         scrolly = Scrollbar(item_frame, orient=VERTICAL)
 
-        self.ItemTable = ttk.Treeview(item_frame, columns=("ItemID", "Name", "Price", "Quantity", "Manufacturer"),
+        self.ItemTable = ttk.Treeview(item_frame, columns=("Sifra", "Ime", "Cena", "Kolicina", "Proizvodjac"),
                                       yscrollcommand=scrolly.set)
         scrolly.pack(side=RIGHT, fill=Y)
         scrolly.config(command=self.ItemTable.yview)
 
         self.ItemTable["show"] = 'headings'
 
-        self.ItemTable.heading("ItemID", text='ID')
-        self.ItemTable.heading("Name", text='Name')
-        self.ItemTable.heading("Price", text='Price')
-        self.ItemTable.heading("Quantity", text='Quantity')
-        self.ItemTable.heading("Manufacturer", text='Manufacturer')
+        self.ItemTable.heading("Sifra", text='Sifra')
+        self.ItemTable.heading("Ime", text='Ime')
+        self.ItemTable.heading("Cena", text='Cena')
+        self.ItemTable.heading("Kolicina", text='Kolicina')
+        self.ItemTable.heading("Proizvodjac", text='Proizvodjac')
 
-        self.ItemTable.column("ItemID", width=90)
-        self.ItemTable.column("Name", width=100)
-        self.ItemTable.column("Price", width=100)
-        self.ItemTable.column("Quantity", width=100)
-        self.ItemTable.column("Manufacturer", width=100)
+        self.ItemTable.column("Sifra", width=90)
+        self.ItemTable.column("Ime", width=100)
+        self.ItemTable.column("Cena", width=100)
+        self.ItemTable.column("Kolicina", width=100)
+        self.ItemTable.column("Proizvodjac", width=100)
         self.ItemTable.pack(fill=BOTH, expand=1)
         self.ItemTable.bind("<ButtonRelease-1>", self.get_data)
 
@@ -122,12 +122,12 @@ class MainFrame:
         cur = con.cursor()
         try:
             if self.var_itemID.get() == "":
-                messagebox.showerror("Error", "Item ID must be required", parent=self.root)
+                messagebox.showerror("Greska", "Potrebno je ubaciti Sifru!", parent=self.root)
             else:
                 cur.execute("Select * from items where itemID=?", (self.var_itemID.get(),))
                 row = cur.fetchone()
                 if row != None:
-                    messagebox.showerror("Error", "This item ID already assigned, try diffrent", parent=self.root)
+                    messagebox.showerror("Greska", "Izabrali ste postojecu Sifru, izaberite drugi", parent=self.root)
                 else:
                     cur.execute("Insert into items (ItemID,Name,Price,Quantity,Manufacturer) values(?,?,?,?,?)", (
                         self.var_itemID.get(),
@@ -137,10 +137,10 @@ class MainFrame:
                         self.var_manufacturer.get()
                     ))
                     con.commit()
-                    messagebox.showinfo('Success', "Item added successfully", parent=self.root)
+                    messagebox.showinfo('Uspesno', "Proizvod dodat uspesno", parent=self.root)
                     self.show()
         except Exception as ex:
-            messagebox.showerror("Error", f"Error due to : {str(ex)}", parent=self.root)
+            messagebox.showerror("Greska", f"Greska od : {str(ex)}", parent=self.root)
 
     # =========================show data method
     def show(self):
@@ -153,7 +153,7 @@ class MainFrame:
             for row in rows:
                 self.ItemTable.insert('', END, values=row)
         except Exception as ex:
-            messagebox.showerror("Error", f"Error due to : {str(ex)}", parent=self.root)
+            messagebox.showerror("Greska", f"Greska od : {str(ex)}", parent=self.root)
 
     # ====================get data method
     def get_data(self, ev):
@@ -172,12 +172,12 @@ class MainFrame:
         cur = con.cursor()
         try:
             if self.var_itemID.get() == "":
-                messagebox.showerror("Error", "Item ID must be required", parent=self.root)
+                messagebox.showerror("Greska", "Potrebana je Sifra", parent=self.root)
             else:
                 cur.execute("Select * from items where itemID=?", (self.var_itemID.get(),))
                 row = cur.fetchone()
                 if row == None:
-                    messagebox.showerror("Error", "This item ID is invalid", parent=self.root)
+                    messagebox.showerror("Greska", "Ova sifra nije validna", parent=self.root)
                 else:
                     cur.execute("Update items set Name=?,Price=?,Quantity=?,Manufacturer=? where ItemID=?", (
                         self.var_name.get(),
@@ -187,10 +187,10 @@ class MainFrame:
                         self.var_itemID.get(),
                     ))
                     con.commit()
-                    messagebox.showinfo('Success', "Item updated successfully", parent=self.root)
+                    messagebox.showinfo('Uspesno', "Proizvod azuriran uspesno", parent=self.root)
                     self.show()
         except Exception as ex:
-            messagebox.showerror("Error", f"Error due to : {str(ex)}", parent=self.root)
+            messagebox.showerror("Greska", f"Greska od : {str(ex)}", parent=self.root)
 
     # =====================delete data method
 
@@ -199,22 +199,22 @@ class MainFrame:
         cur = con.cursor()
         try:
             if self.var_itemID.get() == "":
-                messagebox.showerror("Error", "Item ID must be required", parent=self.root)
+                messagebox.showerror("Greska", "Potrebana je sifra", parent=self.root)
             else:
                 cur.execute("Select * from items where itemID=?", (self.var_itemID.get(),))
                 row = cur.fetchone()
                 if row == None:
-                    messagebox.showerror("Error", "This item ID is invalid", parent=self.root)
+                    messagebox.showerror("Greska", "Ova sifra nije validna", parent=self.root)
                 else:
-                    op = messagebox.askyesno("Confirm", "Do you really want to delete?", parent=self.root)
+                    op = messagebox.askyesno("Potvrdi", "Sigurno zelite da obrisite?", parent=self.root)
                     if op == True:
                         cur.execute('delete from items where itemID=?', (self.var_itemID.get(),))
                         con.commit()
-                        messagebox.showinfo("Delete", "Item deleted successfully", parent=self.root)
+                        messagebox.showinfo("Obrisi", "Proizvod je uspesno obrisen", parent=self.root)
                         self.show()
                         self.clear()
         except Exception as ex:
-            messagebox.showerror("Error", f"Error due to : {str(ex)}", parent=self.root)
+            messagebox.showerror("Greska", f"Greska od : {str(ex)}", parent=self.root)
 
     def clear(self):
         self.var_itemID.set("")
@@ -230,9 +230,9 @@ class MainFrame:
         cur = con.cursor()
         try:
             if self.var_searchType.get() == "Select":
-                messagebox.showerror("Error", "Selector search my option", parent=self.root)
+                messagebox.showerror("Greska", "Izaberi opciju za pretragu", parent=self.root)
             elif self.var_searchtxt.get() == "":
-                messagebox.showerror("Error", "Search input should be required", parent=self.root)
+                messagebox.showerror("Greska", "mora da se unese text", parent=self.root)
             else:
                 cur.execute(
                     "select * from items where " + self.var_searchType.get() + " LIKE '%" + self.var_searchtxt.get() + "%'")
@@ -242,9 +242,9 @@ class MainFrame:
                     for row in rows:
                         self.ItemTable.insert('', END, values=row)
                 else:
-                    messagebox.showerror("Error", "No record found!", parent=self.root)
+                    messagebox.showerror("Greska", "nema podataka sa ovim zahtevima!", parent=self.root)
         except Exception as ex:
-            messagebox.showerror("Error", f"Error due to : {str(ex)}", parent=self.root)
+            messagebox.showerror("Greska", f"Greska od : {str(ex)}", parent=self.root)
 
 
     def logout(self):
